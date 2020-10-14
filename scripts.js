00aa81745c9d8afdd4afbe29b48a761a8847890e
Vadim Lepiochin <dram@inbox.lt>
2020-10-14 22:47:15 +0300

    done

--- scripts.js
+++ scripts.js
@@ -12,7 +12,20 @@ function addTodoItem(e){
     const newTodoItem = document.createElement('li');
     newTodoItem.textContent = inputValue;
     newTodoItem.classList.add('todo-item');
-    
+
+    const buttonComplete = document.createElement('button');
+    buttonComplete.classList.add('button-complete');
+    buttonComplete.textContent = 'Complete';
+
+    const buttonDelete = document.createElement('button');
+    buttonDelete.classList.add('button-delete');
+    buttonDelete.textContent = 'Delete';
+
+    const inside = document.createElement('inside');
+    inside.appendChild(buttonComplete);
+    inside.appendChild(buttonDelete);
+
+    newTodoItem.appendChild(inside);
     todoList.appendChild(newTodoItem);
 
     inputField.value = null;
